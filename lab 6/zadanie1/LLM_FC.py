d3729ac5964d6896dbd1d6d9ff957adb253a6e3e
nimoCreator <76407460+nimoCreator@users.noreply.github.com>
2025-11-20 23:19:14 +0100

    lab 6

--- lab 6/zadanie1/LLM_FC.py
+++ lab 6/zadanie1/LLM_FC.py
@@ -5,7 +5,7 @@ import requests
 from datetime import datetime, timezone
 from typing import Optional, List, Dict, Any
 
-API_KEY = "AIzaSyBWoUQqQTEvx8gI7hD34Enl6AgV5WELe3g" 
+API_KEY = "" 
 MODEL_NAME = "gemini-2.5-flash"
 API_URL = (
     f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL_NAME}:generateContent?key={API_KEY}"
